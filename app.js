3ee43b5e8c5b275081babcc6d9e8c186cc26c7ca
ZeLars3 <zelt378@gmail.com>
2021-12-23 20:23:11 +0300

    connection established with DB

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const orderRoutes = require('./routes/order');
 const categoryRoutes = require('./routes/category');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 const app = express();
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB...', err));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Yahor:!Yana2016!@cluster0.nvtwf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
