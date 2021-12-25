66a7ad267cea691537e0c5e34f57c9528a9f4d71
ZeLars3 <zelt378@gmail.com>
2021-12-25 22:39:05 +0300

    implementing login

--- controller/auth.js
+++ controller/auth.js
@@ -1,14 +1,58 @@
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password,
-        },
-    });
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const  keys = require('../config/keys');
+
+module.exports.login = async function(req, res) {
+    const person = await User.findOne({ email: req.body.email });
+    if (!person) {
+        res.status(404).json({
+            message: 'User with this email does not exist',
+        });
+    }
+    else {
+        const result = await bcrypt.compare(req.body.password, person.password);
+        if (result) {
+            const token = jwt.sign({
+                id: person._id, 
+                email: person.email,
+            }, keys.jwt, { expiresIn: '1h' });
+
+            res.status(200).json({
+                message: 'User logged in',
+                token: `Bearer ${token}`,
+            });
+        }
+        else {
+            res.status(401).json({
+                message: 'Password is incorrect',
+            });
+        }
+    }
 };
 
-module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: 'You are in register page',
-    });
+module.exports.register = async function(req, res) {
+  const person = await User.findOne({ email: req.body.email });
+
+    if (person) {
+        res.status(409).json({
+            message: 'User with this email already exists',
+        });
+    }
+    else {
+        const salt = await bcrypt.genSalt(10);
+        const password = await bcrypt.hash(req.body.password, salt);
+        const user = new User({
+            email: req.body.email,
+            password: password,
+        });
+        try {
+            const result = await user.save();
+            res.status(201).json(result);
+        } catch (err) {
+            res.status(500).json({
+                error: err,
+            });
+        }
+    }
 };
